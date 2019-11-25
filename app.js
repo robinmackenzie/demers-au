4c92ca6defe6f703ac2a663e9fbfc5216e7df983
Robin Mackenzie <rtmackenzie@gmail.com>
2019-11-25 13:59:45 +1100

    Add reset button

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", initialDraw);
 document.getElementById("runButton").addEventListener("click", drawMap);
+document.getElementById("resetButton").addEventListener("click", resetParams);
 
 async function initialDraw() {
   await demers.buildGraph(false);
@@ -16,4 +17,11 @@ async function drawMap() {
     "svgWidth": document.getElementById("wrapper").clientWidth
   }
   await demers.redraw(opts);
+}
+
+function resetParams() {
+  document.getElementById("useLinks").checked = true;
+  document.getElementById("nodeSize").value = 20;
+  document.getElementById("linkForceStrength").value = 0.6;
+  document.getElementById("collisionForceFactor").value = 1;
 }
